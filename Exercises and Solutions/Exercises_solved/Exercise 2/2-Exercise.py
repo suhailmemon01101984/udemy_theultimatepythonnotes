b3c16bec2f155cf5f2d981b18cccb100373d86e4
suhail01101984 <suhailmemon84@gmail.com>
2023-05-10 19:53:35 -0600

    solved exercises 1 -3

--- Exercises and Solutions/Exercises_solved/Exercise 2/2-Exercise.py
+++ Exercises and Solutions/Exercises_solved/Exercise 2/2-Exercise.py
@@ -15,10 +15,13 @@ def multiply_two_numbers(num1,num2):
     return(num1*num2)
 
 def divide_two_numbers(num1,num2):
-    return(num1/num2)
+    if num2==0:
+        raise ZeroDivisionError("divide by zero exception. denominator needs to be non zero")
+    return(num1 / num2)
 
 print(add_two_numbers(10,5))
 print(subtract_two_numbers(5,10))
 print(multiply_two_numbers(5,10))
 print(divide_two_numbers(1723,4))
 print(divide_two_numbers(4,1024))
+print(divide_two_numbers(4,0))
--- Exercises and Solutions/Exercises_solved/Exercise 3/3-Exercise.py
+++ Exercises and Solutions/Exercises_solved/Exercise 3/3-Exercise.py
@@ -10,10 +10,16 @@ The program should display the results of each operation to the user.
 #Template
 
 # Take user input (2 variable eg. num1, num2)
+num1=input("Please enter first number: ")
+print("thanks for providing your input. The first number you entered is " + num1)
 
+num2=input("Please enter second number: ")
+print("thanks for providing your input. The second number you entered is " + num2)
 
 # add num 1 and num 2
 
+sum_of_two_numbers=float(num1)+float(num2)
 
 # Print the result
 
+print("the sum of these two numbers is: ", sum_of_two_numbers)
