f65e7f662f1324675efcbda687a22cc7d063be74
suhail01101984 <suhailmemon84@gmail.com>
2023-05-21 18:52:03 -0600

    "updated expercis2 intermediate"

--- Intermediate Exercises and Solutions/2/Exercise.py
+++ Intermediate Exercises and Solutions/2/Exercise.py
@@ -17,3 +17,25 @@ Here's an outline of the program:
     If the player guesses correctly, print a congratulatory message and the number of guesses it took.
     If the player runs out of guesses, print a message revealing the original word.
 '''
+
+import random
+list_of_words=["constantine","coconut","americano","espresso"]
+number_of_guesses_ind=0
+game_finish_ind=0
+
+while game_finish_ind==0:
+    index=random.randint(0,3)
+    random_word=list(list_of_words[index])
+    random.shuffle(random_word)
+    jumbled_word=''.join(random_word)
+    guessed_word=input(f"The jumbled word is: {jumbled_word}. Please guess the original word: ")
+    number_of_guesses_ind = number_of_guesses_ind + 1
+    if(guessed_word==list_of_words[index]):
+        print(f"Congratulations, you are correct! It took you {number_of_guesses_ind} guesses to win!")
+        game_finish_ind=1
+    else:
+        print("Sorry, bad guess. Please try again")
+
+
+
+
